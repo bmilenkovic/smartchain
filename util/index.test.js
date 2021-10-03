39a11b03af3789b4505f1b9219d4564554f46d89
Branislav Milenkovic <branislav.milenkovic@gmail.com>
2021-10-02 20:30:10 -0500

    3.15 Hashing with Keccak hash

--- util/index.test.js
+++ util/index.test.js
@@ -1,4 +1,4 @@
-const { sortCharacters } = require('./index');
+const { sortCharacters, keccakHash } = require('./index');
 
 describe('util', () => {
     describe('sortCharacters()', () => {
@@ -12,4 +12,12 @@ describe('util', () => {
                 .not.toEqual(sortCharacters({ bar: 'bar' }));
         });
     });
+
+    describe('keccakHash()', () => {
+        it('produces a keccak256 hash', () => {
+            expect(keccakHash('foo'))
+                .toEqual('b2a7ad9b4a2ee6d984cc5c2ad81d0c2b2902fa410670aa3f2f4f668a1f80611c');
+        });
+    });
+
 });
