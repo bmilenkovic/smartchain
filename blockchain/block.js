d91879f147a28bd2ac719624a26f9aecf8e9c41a
Branislav Milenkovic <branislav.milenkovic@gmail.com>
2021-10-08 22:09:49 -0500

    3.19 Mine block with test

--- blockchain/block.js
+++ blockchain/block.js
@@ -1,31 +1,59 @@
 const { GENESIS_DATA } = require('../config');
+const { keccakHash } = require('../util');
 
 const HASH_LENGTH = 64;
 const MAX_HASH_VALUE = parseInt('f'.repeat(HASH_LENGTH), 16);
+const MAX_NONCE_VALUE = 2 ** 64;
 
 class Block {
-    constructor({ blockHeaders }) {
-        this.blockHeaders = blockHeaders;
+  constructor({ blockHeaders }) {
+    this.blockHeaders = blockHeaders;
 
+  }
+
+  static calculateBlockTargetHash({ lastBlock }) {
+    const value = (MAX_HASH_VALUE / lastBlock.blockHeaders.difficulty).toString(16);
+
+    if (value.length > HASH_LENGTH) {
+      return 'f'.repeat(HASH_LENGTH);
     }
 
-    static calculateBlockTargetHash({ lastBlock }) {
-        const value = (MAX_HASH_VALUE / lastBlock.blockHeaders.difficulty).toString(16);
-    
-        if (value.length > HASH_LENGTH) {
-          return 'f'.repeat(HASH_LENGTH);
-        }
-    
-        return '0'.repeat(HASH_LENGTH - value.length) + value;
+    return '0'.repeat(HASH_LENGTH - value.length) + value;
+  }
+
+  static mineBlock({ lastBlock, beneficiary }) {
+    const target = Block.calculateBlockTargetHash({ lastBlock });
+    let timestamp, truncatedBlockHeaders, header, nonce, underTargetHash;
+
+    do {
+      timestamp = Date.now();
+      truncatedBlockHeaders = {
+        parentHash: keccakHash(lastBlock.blockHeaders),
+        beneficiary,
+        difficulty: lastBlock.blockHeaders.difficulty + 1,
+        number: lastBlock.blockHeaders.number + 1,
+        timestamp
+      };
+      header = keccakHash(truncatedBlockHeaders);
+      nonce = Math.random() * MAX_NONCE_VALUE;
+
+      underTargetHash = keccakHash(header + nonce);
+    } while (underTargetHash > target);
+
+
+    return new this({
+      blockHeaders: {
+        Block: { ...truncatedBlockHeaders, nonce }
       }
+    });
 
-    static mineBlock({ lastBlock, beneficiery }) { 
 
-    }
 
-    static genesis() {
-        return new this(GENESIS_DATA);
-    }
+  }
+
+  static genesis() {
+    return new this(GENESIS_DATA);
+  }
 }
 
 
