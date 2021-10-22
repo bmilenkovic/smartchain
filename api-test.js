035bd907156df08a0cb7583a8a31b0416dc43864
Branislav Milenkovic <branislav.milenkovic@gmail.com>
2021-10-22 17:15:56 -0500

    5.37 Transact endpoint

---
+++ api-test.js
@@ -0,0 +1,35 @@
+const { throwStatement } = require('@babel/types');
+const request = require('request');
+
+const BASE_URL = 'http://localhost:3000';
+
+const postTransact = ({ code, to, value, gasLimit }) => {
+    return new Promise((resolve, reject) => {
+        request(`${BASE_URL}/account/transact`, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ code, to, value, gasLimit })
+        }, (error, response, body) => {
+            return resolve(JSON.parse(body));
+        });
+    });
+}
+
+postTransact({})
+    .then(postTransactResponse => {
+        console.log(
+            'postTransactResponse (Create Account Transaction)',
+            postTransactResponse
+        );
+
+        toAccountData = postTransactResponse.transaction.data.accountData;
+
+        return postTransact({ to: toAccountData.address, value: 20 });
+    })
+    .then(postTransactResponse2 => {
+        console.log(
+            'postTransactResponse2 (Standard Transaction)',
+            postTransactResponse2
+        );
+
+    })
--- api/index.js
+++ api/index.js
@@ -1,12 +1,24 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const request = require('request');
 const Blockchain = require('../blockchain');
 const Block = require('../blockchain/block');
 const PubSub = require('./pubsub');
+const TransactionQueue = require('../transaction/transaction-queue');
+const Account = require('../account');
+const Transaction = require('../transaction');
+
 
 const app = express();
+app.use(bodyParser.json());
+
 const blockchain = new Blockchain();
+const transactionQueue = new TransactionQueue();
 const pubsub = new PubSub({ blockchain });
+const account = new Account();
+const transaction = Transaction.createTransaction({ account });
+
+transactionQueue.add(transaction);
 
 app.get('/blockchain', (req, res, next) => {
     const { chain } = blockchain;
@@ -16,7 +28,10 @@ app.get('/blockchain', (req, res, next) => {
 
 app.get('/blockchain/mine', (req, res, next) => {
     const lastBlock = blockchain.chain[blockchain.chain.length - 1];
-    const block = Block.mineBlock({ lastBlock });
+    const block = Block.mineBlock({
+        lastBlock,
+        beneficiary: account.address
+    });
 
     blockchain.addBlock({ block })
         .then(() => {
@@ -25,6 +40,19 @@ app.get('/blockchain/mine', (req, res, next) => {
         })
         .catch(next);
 
+});
+
+app.post('/account/transact', (req, res, next) => {
+    const { to, value } = req.body;
+    const transaction = Transaction.createTransaction({
+        account: !to ? new Account() : account,
+        to,
+        value
+    });
+
+    transactionQueue.add(transaction);
+
+    res.json({ transaction });
 });
 
 app.use((err, req, res, next) => {
